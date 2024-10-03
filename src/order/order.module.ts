f64e7d4c75c4cc309cb3c300ce4d4845b369e77c
mahasegni.inbox@gmail.com <MahaSegni>
2024-10-03 14:29:06 +0200

    test create order

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,16 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import OrderController from './infrastructure/presentation/order.controller';
 import { Order } from './domain/entity/order.entity';
 import { OrderItem } from './domain/entity/order-item.entity';
-import OrderRepositoryTypeOrm from 'src/order/infrastructure/persistance/order.repository';
 import { PdfGeneratorService } from './infrastructure/pdf/pdf-generator.service';
 import { GenerateInvoiceService } from './application/use_case/generate-invoice.service';
-import { OrderRepositoryInterface } from './domain/port/order.repository.interface';
+import { OrderRepositoryInterface } from './domain/port/persistance/order.repository.interface';
 import { PayOrderService } from './application/use_case/pay-order.service';
 import { CancelOrderService } from './application/use_case/cancel-order.service';
 import { SetInvoiceAddressOrderService } from './application/use_case/set-invoice-address.service';
 import { SetShippingAddressOrderService } from './application/use_case/set-shipping-address-order.service';
 import { CreateOrderService } from './application/use_case/create-order.service';
 import { PdfGeneratorServiceInterface } from './domain/port/pdf/pdf-generator.service.interface';
+import OrderRepositoryTypeOrm from './infrastructure/persistance/order.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order, OrderItem])],
